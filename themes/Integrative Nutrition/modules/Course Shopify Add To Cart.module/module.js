5ea788876eacb9c3bdae089f738984acdf78b9e0
Alex Orzeck-Byrnes <alex.orzeckbyrnes@integrativenutrition.com>
2024-04-22 10:52:35 -0400

    chore: lint and refactor course shopify add to cart module

--- themes/Integrative Nutrition/modules/Course Shopify Add To Cart.module/module.js
+++ themes/Integrative Nutrition/modules/Course Shopify Add To Cart.module/module.js
@@ -1,35 +1,76 @@
 (function () {
-  var selectedOptions = {};
+  let moduleData = {};
+
+  try {
+    const rawData = window.sessionStorage.getItem('course_shopify_add_to_cart');
+
+    moduleData = JSON.parse(rawData);
+  } catch (e) {
+    console.error(e);
+    return;
+  }
+
+  const msToCloseAddPopUp = 8000;
+  const optionKeys = [];
+  const selectedOptions = {};
   let product;
-  let optionKeys = [];
-  const msToCloseAddPopUp = 5000;
-  
+
+  function getValidVariant(productData) {
+    let validVariant;
+
+    for (const variant of productData.variants) {
+      if (variant.available) {
+        let isMatch = true;
+
+        variant.selectedOptions?.forEach(({ name, value }) => {
+          if (selectedOptions[name] !== value) {
+            isMatch = false;
+          }
+        });
+
+        if (isMatch) {
+          validVariant = variant;
+        }
+      }
+    }
+
+    return validVariant;
+  }
+
   // Update selectedOptions and show/hide buttons when an attribute is changed
   function checkSelectedOptions(changedAttribute) {
     const options = {};
-    for(const v of product.variants) {
-      if(v.available) {
+
+    for (const variant of product.variants) {
+      if (variant.available) {
         let isValid = false;
+
         // Loading all for changed field
-        for(const selectedOption of v.selectedOptions) {
-          if(selectedOption.name == changedAttribute) {
-            if(!options[selectedOption.name]) {
-              options[selectedOption.name] = [ selectedOption.value ];
-            } else if(!options[selectedOption.name].includes(selectedOption.value)) {
+        for (const selectedOption of variant.selectedOptions) {
+          if (selectedOption.name === changedAttribute) {
+            if (!options[selectedOption.name]) {
+              options[selectedOption.name] = [selectedOption.value];
+            } else if (
+              !options[selectedOption.name].includes(selectedOption.value)
+            ) {
               options[selectedOption.name].push(selectedOption.value);
             }
-            if(selectedOption.value == selectedOptions[changedAttribute]) {
+
+            if (selectedOption.value === selectedOptions[changedAttribute]) {
               isValid = true;
             }
           }
         }
+
         // Only loading options if valid relative to the changed option
-        if(isValid) {
-          for(const selectedOption of v.selectedOptions) {
-            if(selectedOption.name != changedAttribute) {
-              if(!options[selectedOption.name]) {
-                options[selectedOption.name] = [ selectedOption.value ];
-              } else if(!options[selectedOption.name].includes(selectedOption.value)) {
+        if (isValid) {
+          for (const selectedOption of variant.selectedOptions) {
+            if (selectedOption.name !== changedAttribute) {
+              if (!options[selectedOption.name]) {
+                options[selectedOption.name] = [selectedOption.value];
+              } else if (
+                !options[selectedOption.name].includes(selectedOption.value)
+              ) {
                 options[selectedOption.name].push(selectedOption.value);
               }
             }
@@ -37,173 +78,252 @@
         }
       }
     }
-    for(const key in options) {
-      if(!options[key].includes(selectedOptions[key])) {
-        selectedOptions[key] = options[key][0];
+
+    Object.entries(options).forEach(([key, value]) => {
+      if (!value.includes(selectedOptions[key])) {
+        [selectedOptions[key]] = value;
       }
-    }
-    console.log("OPTIONS");
+    });
+
+    // TODO: remove
+    console.log('OPTIONS');
     console.log(options);
-    
-    console.log("SELECTED OPTIONS");
+
+    // TODO: remove
+    console.log('SELECTED OPTIONS');
     console.log(selectedOptions);
-    
+
     // Original buttons
     /*
     let html = '';
+
     // Not showing options if there is only 1 default field
-    if(options[optionKeys[0]][0] != 'Default Title') {
-      for(const o of optionKeys) {
-        html += `<div class="jd-buy-option-label">${o}</div>`;
+    if (options[optionKeys[0]][0] !== 'Default Title') {
+      for (const optionKey of optionKeys) {
+        html += `<div class="jd-buy-option-label">${optionKey}</div>`;
         html += '<div class="jd-shopify-option-wrap">';
-        for(let i = 0; i < options[o].length; i++) {
-          const val = options[o][i];
-          html += `<div data-shopify-option-val="${val}" data-shopify-option-type="${o}" class="${val == selectedOptions[o] ? 'jd-shopify-option-selected' : ''} jd-shopify-option jd-request-btn hs-button jd-gray-btn">${val}</div>`;
+
+        for (let i = 0; i < options[optionKey].length; i++) {
+          const val = options[optionKey][i];
+
+          html += `
+            <div
+              data-shopify-option-val="${val}"
+              data-shopify-option-type="${optionKey}"
+              class="jd-shopify-option jd-request-btn hs-button jd-gray-btn${val === selectedOptions[optionKey] ? ' jd-shopify-option-selected' : ''}"
+            >
+              ${val}
+            </div>
+          `;
         }
+
         html += '</div>';
       }
     }
-    html += '<div class="jd-shopify-add-btn jd-request-btn hs-button light-button">Add to Cart</div>';
+
+    html += `
+      <button
+        class="jd-shopify-add-btn jd-request-btn hs-button light-button"
+        type="button"
+      >
+        Add to Cart
+      </button>
+    `;
     */
-    
+
     // Checkboxes
     let html = '<div class="jd-shopify-options">';
+
     // Not showing options if there is only 1 default field
-    if(options[optionKeys[0]][0] != 'Default Title') {
-      for(const o of optionKeys) {
-        html += `<div><div class="jd-buy-option-label">${o}</div>`;
-        html += '<div class="jd-shopify-option-wrap">';
-        for(let i = 0; i < options[o].length; i++) {
-          const val = options[o][i];
-          html += `<div><input id="${o + '_' + val}" value="${val}" name="${o}" ${val == selectedOptions[o] ? 'checked="true"' : ''} type="radio" />`;
-          html += `<label for="${o + '_' + val}">${val}</label></div>`;
+    if (options[optionKeys[0]][0] !== 'Default Title') {
+      for (const optionKey of optionKeys) {
+        html += `
+          <div>
+            <div class="jd-buy-option-label">${optionKey}</div>
+            <div class="jd-shopify-option-wrap">
+        `;
+
+        const values = options[optionKey];
+
+        for (const value of values) {
+          html += `
+            <div>
+              <input
+                id="${optionKey}_${value}"
+                value="${value}"
+                name="${optionKey}"
+                ${value === selectedOptions[optionKey] ? 'checked="true"' : ''}
+                type="radio"
+              />
+              <label for="${optionKey}_${value}">${value}</label>
+            </div>
+          `;
         }
+
         html += '</div></div>';
       }
     }
-    html += '</div><div class="jd-shopify-add-btn jd-request-btn hs-button light-button">Add to Cart</div>';
-    html += '<div style="display:none" class="jd-shopify-add-exists-msg">This course is already in your cart</div>';
 
-    $('#{{name}}').html(html);
-      
-      // Option click button
-      /*
-      $('#{{name}} .jd-shopify-option').click(function() {
-        if($(this).hasClass('jd-shopify-option-selected')) {
-          return;
-        }
-        const type = $(this).data('shopify-option-type');
-        const val = $(this).data('shopify-option-val');
-        selectedOptions[type] = val;
-        checkSelectedOptions(type);
-      });
-      */
-      
-      // Option click checkbox
-      $('#{{name}} .jd-shopify-option-wrap input[type=radio]').change(function() {
+    html += `
+      </div>
+      <button
+        class="jd-shopify-add-btn jd-request-btn hs-button light-button"
+        type="button"
+      >
+        Add to Cart
+      </button>
+      <div style="display: none" class="jd-shopify-add-exists-msg">
+        This course is already in your cart
+      </div>
+    `;
+
+    $(`#${moduleData.name}`).html(html);
+
+    // Option click button
+    /*
+    $(`#${moduleData.name} .jd-shopify-option`).click(function() {
+      if ($(this).hasClass('jd-shopify-option-selected')) {
+        return;
+      }
+
+      const type = $(this).data('shopify-option-type');
+      const val = $(this).data('shopify-option-val');
+
+      selectedOptions[type] = val;
+      checkSelectedOptions(type);
+    });
+    */
+
+    // Option click checkbox
+    $(`#${moduleData.name} .jd-shopify-option-wrap input[type=radio]`).change(
+      function () {
         const type = $(this).attr('name');
         const val = $(this).val();
+
         selectedOptions[type] = val;
         checkSelectedOptions(type);
-      });
-  
-      $(`.jd-shopify-option-wrap input[name="${changedAttribute}"]`).first().focus()
-  
-      // Add to cart click
-      $('#{{name}} .jd-shopify-add-btn').click(async function() {
-          let variant;
-          for(const v of product.variants) {
-            if(v.available) {
-              let isMatch = true;
-              for(const selectedOption of v.selectedOptions) {
-                if(selectedOptions[selectedOption.name] != selectedOption.value) {
-                  isMatch = false;
-                }
-              }
-              if(isMatch) {
-                variant = v;
-              }
+      }
+    );
+
+    $(`.jd-shopify-option-wrap input[name="${changedAttribute}"]`)
+      .first()
+      .focus();
+
+    const addToCart = async (productData) => {
+      // TODO: remove
+      console.log({ productData });
+
+      const variant = getValidVariant(productData);
+
+      if (!variant) {
+        alert('This combination of options is invalid');
+      } else {
+        let alreadyInCart = false;
+
+        const cartCookie = getCookie('shopifyCart');
+        const checkout = await IINShopifyClient.checkout.fetch(cartCookie);
+
+        for (const lineItem of checkout.lineItems) {
+          if (lineItem.variant.product.id === productData.id) {
+            alreadyInCart = true;
+            break;
+          }
+        }
+
+        if (alreadyInCart) {
+          $(`#${moduleData.name} .jd-shopify-add-exists-msg`).show();
+          return;
+        }
+
+        $(`#${moduleData.name} .jd-shopify-add-exists-msg`).hide();
+
+        const lineItemsToAdd = [
+          {
+            variantId: variant.id,
+            quantity: 1,
+          },
+        ];
+
+        try {
+          const updatedCheckout = await IINShopifyClient.checkout.addLineItems(
+            cartCookie,
+            lineItemsToAdd
+          );
+
+          updateCartTotal(updatedCheckout);
+
+          $('.jd-header-wrap').addClass('jd-scrolled').removeClass('ishidden');
+          $('.jd-blackout').addClass('jd-blackout-show');
+          $('.jd-add-pop .jd-add-pop-cat').text(`${moduleData.category}`);
+          $('.jd-add-pop .jd-add-pop-name').text(`${moduleData.courseName}`);
+          $('.jd-add-pop .jd-add-pop-img > div').attr(
+            'style',
+            `background: url(${variant.image.src})`
+          );
+
+          let optionsHTML = '';
+
+          Object.entries(selectedOptions).forEach(([key, value]) => {
+            if (value) {
+              optionsHTML += `<div><strong>${key}:</strong> ${value}</div>`;
             }
-          }
+          });
 
-          console.log(product);
-  
-          if(!variant) {
-            alert('This combination of options is invalid')
-          } else {
-            let alreadyInCart = false;
-            await IINShopifyClient.checkout.fetch(getCookie('shopifyCart'))
-              .then(checkout => {
-                for(const lineItem of checkout.lineItems) {
-                  if(lineItem.variant.product.id == product.id) {
-                    alreadyInCart = true;
-                    break;
-                  }
-                }
-              })
-            
-            if(alreadyInCart) {
-              $('#{{name}} .jd-shopify-add-exists-msg').show();
-              return;
-            }            
-            $('#{{name}} .jd-shopify-add-exists-msg').hide();
-            
-            const lineItemsToAdd = [
-              {
-                variantId: variant.id,
-                quantity: 1
-              }
-            ];
-            IINShopifyClient.checkout.addLineItems(getCookie('shopifyCart'), lineItemsToAdd)
-              .then((checkout) => {
-                console.log(checkout);
-                updateCartTotal(checkout);
-                $('.jd-header-wrap').addClass('jd-scrolled').removeClass('ishidden');
-                $('.jd-blackout').addClass('jd-blackout-show');
-                $('.jd-add-pop .jd-add-pop-cat').text('{{ module.category }}');
-                $('.jd-add-pop .jd-add-pop-name').text('{{ module.course_name }}');
-                $('.jd-add-pop .jd-add-pop-img > div').attr('style', 'background: url(' + variant.image.src + ')');
-                let optionsHTML = '';
-                for(const o in selectedOptions) {
-                  optionsHTML += `<div><b>${o}:</b> ${selectedOptions[o]}</div>`;
-                }
-                $('.jd-add-pop .jd-add-pop-options').html(optionsHTML);
-                $('.jd-add-pop .jd-add-pop-price').text('$' + parseFloat(variant.price.amount).toLocaleString());
+          $('.jd-add-pop .jd-add-pop-options').html(optionsHTML);
+          $('.jd-add-pop .jd-add-pop-price').text(
+            `$${parseFloat(variant.price.amount).toLocaleString()}`
+          );
+          $('.jd-add-pop').css(
+            'top',
+            `${$('.jd-header-wrap').outerHeight()}px`
+          );
+          $('.jd-add-pop').addClass('jd-add-pop-show');
 
-                $('.jd-add-pop').css('top', $('.jd-header-wrap').outerHeight() + 'px')
-                 $('.jd-add-pop').addClass('jd-add-pop-show');
-                setTimeout(() => {
-                  $('.jd-blackout').removeClass('jd-blackout-show');
-                  $('.jd-add-pop').removeClass('jd-add-pop-show');
-                }, msToCloseAddPopUp);
-              })
-              .catch(e => {
-                console.log(e);
-              });
-          }
-        
-      });
+          setTimeout(() => {
+            $('.jd-blackout').removeClass('jd-blackout-show');
+            $('.jd-add-pop').removeClass('jd-add-pop-show');
+          }, msToCloseAddPopUp);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    };
+
+    // Add to cart click
+    $(`#${moduleData.name} .jd-shopify-add-btn`).click(function () {
+      addToCart(product);
+    });
   }
-  
-  IINShopifyClient.product.fetch('gid://shopify/Product/{{ module.product_id }}').then(function(p) {
-      product = p;
-      const options = {};
+
+  async function getProductData() {
+    try {
+      const gidPath = `gid://shopify/Product/${moduleData.productID}`;
+
+      product = await IINShopifyClient.product.fetch(gidPath);
+
       let initialOptionToCheck;
-      for(const v of product.variants) {
-        if(v.available) {
-          for(const selectedOption of v.selectedOptions) {
-            if(!initialOptionToCheck) {
-              initialOptionToCheck = selectedOption.name;
-              selectedOptions[selectedOption.name] = selectedOption.value;
+
+      for (const variant of product.variants) {
+        if (variant.available) {
+          for (const { name, value } of variant.selectedOptions) {
+            if (!initialOptionToCheck) {
+              initialOptionToCheck = name;
+              selectedOptions[name] = value;
             }
+
             // Keeping keys in array to preserve order
-            optionKeys.push(selectedOption.name);
+            optionKeys.push(name);
           }
+
           break;
         }
       }
-        
+
       checkSelectedOptions(initialOptionToCheck);
-    })
-    .catch(e => { console.log(e); });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  getProductData();
 })();
