546458584696a24336fafc63255c657c4070c291
Alex Orzeck-Byrnes <alex.orzeckbyrnes@integrativenutrition.com>
2024-07-15 10:59:02 -0400

    chore: linted and prettier-ified datalayer_ecomm.js

--- themes/Integrative Nutrition/js/datalayer_ecomm.js
+++ themes/Integrative Nutrition/js/datalayer_ecomm.js
@@ -1,10 +1,10 @@
 window.dataLayer = window.dataLayer || [];
 
 const triggerECommEvent = async (rawPayload = {}) => {
-    if (IIN.cookies.checkCookie('analyticstest')) {
-        if (rawPayload && rawPayload.event) {
-            window.dataLayer.push({ ecommerce: null }); // As per GTM need-resetting
-            window.dataLayer.push(rawPayload);
-        }
+  if (IIN.cookies.checkCookie('analyticstest')) {
+    if (rawPayload && rawPayload.event) {
+      window.dataLayer.push({ ecommerce: null }); // As per GTM need-resetting
+      window.dataLayer.push(rawPayload);
     }
+  }
 };
