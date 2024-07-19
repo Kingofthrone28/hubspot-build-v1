f6769ae4c28bfb8f53e66df755b0e68133e3ba29
Alex Orzeck-Byrnes <alex.orzeckbyrnes@integrativenutrition.com>
2024-07-19 13:53:32 -0400

    chore: linted and formatted form-phone-validation

--- themes/Integrative Nutrition/js/form-phone-validation.js
+++ themes/Integrative Nutrition/js/form-phone-validation.js
@@ -1,44 +1,46 @@
 (function () {
-  function validatePhones(formId, e) {
+  function validatePhones(formId, event) {
     let allNumbersValid = true;
 
     // Validate each intl phone field
     $(`${formId} .hs-fieldtype-intl-phone`).each(function () {
-      const countryCode = $(this).find("select").first().val();
-      const numberInput = $(this).find("input[type=tel]").first();
+      const countryCode = $(this).find('select').first().val();
+      const numberInput = $(this).find('input[type=tel]').first();
       let isValid = false;
+
       // phoneUtils comes from libphonenumber.js
       try {
         isValid = phoneUtils.isValidNumber(
-          numberInput.val().replace(/[^0-9+]/g, ""),
-          countryCode
+          numberInput.val().replace(/[^0-9+]/g, ''),
+          countryCode,
         );
-      } catch (e) {
-        console.log(e);
+      } catch (error) {
+        console.log(error);
       }
+
       if (isValid) {
-        $(this).closest(".hs-form-field").find(".lpn-invalid-msg").hide();
-        numberInput.removeClass("invalid");
-        numberInput.removeClass("error");
+        $(this).closest('.hs-form-field').find('.lpn-invalid-msg').hide();
+        numberInput.removeClass('invalid');
+        numberInput.removeClass('error');
       } else {
         allNumbersValid = false;
-        $(this).closest(".hs-form-field").find(".lpn-invalid-msg").show();
-        numberInput.addClass("invalid");
-        numberInput.addClass("error");
+        $(this).closest('.hs-form-field').find('.lpn-invalid-msg').show();
+        numberInput.addClass('invalid');
+        numberInput.addClass('error');
       }
     });
 
     // Prevent default if any phone field is invalid
     if (!allNumbersValid) {
-      e.preventDefault();
+      event.preventDefault();
     }
   }
 
-  window.addEventListener("message", (event) => {
+  window.addEventListener('message', (event) => {
     // Form ready event callback
     if (
-      event.data.type === "hsFormCallback" &&
-      event.data.eventName === "onFormReady"
+      event.data.type === 'hsFormCallback' &&
+      event.data.eventName === 'onFormReady'
     ) {
       const formId = `#hsForm_${event.data.id}_${event.target.options.formInstanceId}`;
 
@@ -50,7 +52,7 @@
         });
 
         // Enter key pressed
-        $(`${formId}`).on("keypress", (e) => {
+        $(`${formId}`).on('keypress', (e) => {
           if (e.keyCode === 13) {
             validatePhones(formId, e);
           }
@@ -59,9 +61,9 @@
         // Add custom invalid message
         $(`${formId} .hs-fieldtype-intl-phone`).each(function () {
           $(this)
-            .closest(".input")
+            .closest('.input')
             .after(
-              '<div style="display:none" class="lpn-invalid-msg">The phone number entered is invalid</div>'
+              '<div style="display:none" class="lpn-invalid-msg">The phone number entered is invalid</div>',
             );
         });
       }
