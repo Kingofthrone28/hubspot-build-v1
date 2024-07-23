626908fcdc4ffea6e84c16b95bdb4bd74b3a89c8
Justin Bernegger <justin.bernegger@Justin-Bernegger-MacBook-Pro.local>
2024-07-23 14:25:11 -0400

    add onFormSubmitted handler for sample class

---
+++ themes/Integrative Nutrition/js/sample-class-form-handler.js
@@ -0,0 +1,21 @@
+/** JS Snippet to be added to a page with a Sample Class Form
+ * Captures form submit event for sample class form and redirects based upon HTML lang
+ */
+
+const SAMPLE_CLASS_FORM_ID = '';
+const ENGLISH_SAMPLE_CLASS_URL = '';
+const SPANISH_SAMPLE_CLASS_URL = '';
+
+window.addEventListener('message', (event) => {
+  if (
+    event.data.type === 'hsFormCallback' &&
+    event.data.eventName === 'onFormSubmitted' &&
+    event.data.id === SAMPLE_FORM_ID
+  ) {
+    if (document.documentElement.lang === 'en') {
+      window.location.href = ENGLISH_SAMPLE_CLASS_URL;
+    } else if (document.documentElement.lang === 'es') {
+      window.location.href = SPANISH_SAMPLE_CLASS_URL;
+    }
+  }
+});
