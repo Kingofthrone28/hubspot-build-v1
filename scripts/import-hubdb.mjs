fa274dcea78d4aaf6774585f8ca3471a0ab89845
Benjamin Gordon <benjamin.gordon8@icloud.com>
2024-08-07 15:37:51 -0400

    added publish hubdb tables
    still not using postHubdbRows function and instead doing it in main

--- scripts/import-hubdb.mjs
+++ scripts/import-hubdb.mjs
@@ -3,11 +3,11 @@ import inquirer from 'inquirer';
 
 const hubdb_names = [
   "course_catalog",
-  // "shopify_bundle_product_recommendations",
-  // "course_catalog_chopra",
-  // "top_bar_schedule",
-  // "course_bundle",
-  // "taxonomy_overrides"
+  "shopify_bundle_product_recommendations",
+  "course_catalog_chopra",
+  "top_bar_schedule",
+  "course_bundle",
+  "taxonomy_overrides"
 ]
 
 if (process.argv[2] && process.argv[3]) {
@@ -35,8 +35,8 @@ function getKeys() {
   inquirer
     .prompt(questions)
     .then((answers) => {
-      const { prodKey } = answers.prodKey;
-      const { devKey } = answers.devKey;
+      const { prodKey } = answers;
+      const { devKey } = answers;
       main(prodKey, devKey)
     })
     .catch((error) => {
@@ -51,32 +51,22 @@ function getKeys() {
 
 async function main(prodKey, devKey) {
   try {
-    // console.log('pagesp', pages)
-    //const hubdb_tables = await getHubdbTables(prodKey)
-    //await postAllHubdbTables(hubdb_tables, devKey)
+    const hubdb_tables = await getHubdbTables(prodKey)
+    await postAllHubdbTables(hubdb_tables, devKey)
     const existingTables = await getHubdbRows(prodKey)
-    console.log('x', existingTables)
+    //await postHubdbRows(existingTables);
     const newTables = existingTables.map(table => {
       return {
         inputs: table
       };
     })
-    // const newTables = existingTables.map(createRows)
-    console.log('new tables', newTables);
     const hubspotClientDev = new hubspot.Client({ accessToken: devKey });
-
     const createPromises = newTables.map((table, index) => {
       const name = hubdb_names[index];
-      console.log('name', name)
-      console.log('table', table)
       return hubspotClientDev.cms.hubdb.rowsBatchApi.createDraftTableRows(name, table);
     })
-
     const results = await Promise.all(createPromises);
-    console.log('res', results)
-
-    // await postHubdbRows(newTables, devKey)
-
+    await publishTables(devKey);
   } catch (error) {
     console.error(error)
   }
@@ -88,11 +78,11 @@ async function getHubdbTables(prodKey) {
   const isGetLocalizedSchema = undefined;
   const archived = undefined;
   const includeForeignIds = undefined;
-  //for (let i = 0; i < hubdb_names.length; i++) {
+  for (let i = 0; i < hubdb_names.length; i++) {
   //const promise = hubspotClientProd.cms.hubdb.tablesApi.getTableDetails(hubdb_names[i]);
-  const promise = hubspotClientProd.cms.hubdb.tablesApi.getTableDetails("top_bar_schedule");
+  const promise = hubspotClientProd.cms.hubdb.tablesApi.getTableDetails(hubdb_names[i]);
   promises.push(promise)
-  //}
+  }
   try {
     const hubdbList = await Promise.all(promises);
     console.info('GET hubdb tables complete')
@@ -107,8 +97,6 @@ async function getHubdbTables(prodKey) {
 async function postAllHubdbTables(hubdb_tables, devKey) {
   const hubspotClientDev = new hubspot.Client({ accessToken: devKey });
   const formRequest = []
-  //const dynamicMetaTags = {};
-  //const HubDbTableV3Request = { dynamicMetaTags, allowPublicApiAccess: false, useForPages: true, columns: [{"id":"1","name":"text_column","type":"TEXT","label":"Text Column"}], name: "test_table", enableChildTablePages: false, label: "Test Table", allowChildTables: true };
   for (const table of hubdb_tables) {
     const promise = hubspotClientDev.cms.hubdb.tablesApi.createTable(table);
     formRequest.push(promise)
@@ -127,9 +115,6 @@ async function postAllHubdbTables(hubdb_tables, devKey) {
 async function getHubdbRows(prodKey) {
   const hubspotClientProd = new hubspot.Client({ accessToken: prodKey });
   const promises = [];
-  const isGetLocalizedSchema = undefined;
-  const archived = undefined;
-  const includeForeignIds = undefined;
   for (let i = 0; i < hubdb_names.length; i++) {
     const tableName = hubdb_names[i]
     const promise = hubspotClientProd.cms.hubdb.rowsApi.readDraftTableRows(tableName);
@@ -148,30 +133,40 @@ async function getHubdbRows(prodKey) {
   }
 }
 
-// async function postHubdbRows(hubdb_rows, devKey) {
-//   const hubspotClientDev = new hubspot.Client({ accessToken: devKey });
-//   const formRequest = []
-//   //const dynamicMetaTags = {};
-//   //const HubDbTableV3Request = { dynamicMetaTags, allowPublicApiAccess: false, useForPages: true, columns: [{"id":"1","name":"text_column","type":"TEXT","label":"Text Column"}], name: "test_table", enableChildTablePages: false, label: "Test Table", allowChildTables: true };
-//   for (let i = 0; i < hubdb_names.length; i++) {
-//     const promise = hubspotClientDev.cms.hubdb.rowsBatchApi.createDraftTableRows(hubdb_names[i], hubdb_rows);
-//     formRequest.push(promise)
-//   }
-
-//   try {
-//     await Promise.all(formRequest)
-//     console.info('POST hubdb rows complete')
-//   }
-//   catch (error) {
-//     console.error('Failed to create hubdb tables: ${error}', error);
-//     throw error;
-//   }
+// async function postHubdbRows(devKey) {
+//     try {
+//     const newTables = existingTables.map(table => {
+//       return {
+//         inputs: table
+//       };
+//     })
+//     const hubspotClientDev = new hubspot.Client({ accessToken: devKey });
+//     const createPromises = newTables.map((table, index) => {
+//       const name = hubdb_names[index];
+//       return hubspotClientDev.cms.hubdb.rowsBatchApi.createDraftTableRows(name, table);
+//     })
+//     const results = await Promise.all(createPromises);
+//     }
+//     catch (error) {
+//         console.error('Failed to create hubdb tables: ${error}', error);
+//         throw error;
+//       }
 // }
 
-
-//main
-//get and post rows
-//for (tables)
-//get rows(id)
-//post rows
-
+async function publishTables(devKey){
+    const promises = [];
+    const hubspotClientDev = new hubspot.Client({ accessToken: devKey });
+    for (let i = 0; i < hubdb_names.length; i++) {
+        const promise = hubspotClientDev.cms.hubdb.tablesApi.publishDraftTable(hubdb_names[i]);
+        promises.push(promise)
+        }
+        try {
+          await Promise.all(promises);
+          console.info('PUBLISH hubdb tables complete')
+        } catch (e) {
+          e.message === 'HTTP request failed'
+            ? console.error(JSON.stringify(e.response, null, 2))
+            : console.error(e)
+        }
+    
+  }
