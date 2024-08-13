bd5bd1eb5948b7c4f0aa3531ca3ffeae2e1f0154
Justin Bernegger <justin.bernegger@Justin-Bernegger-MacBook-Pro.local>
2024-08-13 14:56:55 -0400

    revert revert and fix bug with variant filtering

--- themes/Integrative Nutrition/modules/Course Shopify Add To Cart.module/module.js
+++ themes/Integrative Nutrition/modules/Course Shopify Add To Cart.module/module.js
@@ -86,7 +86,7 @@
   const getOptions = (selection, optionNames, variants) => {
     const options = {};
     const newSelection = {};
-    let filteredVariants = [...variants];
+    let filteredVariants = getPossibleVariants(variants, newSelection);
 
     // Iterate through option names
     optionNames.forEach((optionName) => {
