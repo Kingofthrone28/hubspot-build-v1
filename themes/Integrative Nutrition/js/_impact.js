882c419e66eebefadfd738df7a9b91de51834594
Justin Bernegger <justin.bernegger@Justin-Bernegger-MacBook-Pro.local>
2024-08-21 09:50:47 -0400

    reformat and reference cookie functions

--- themes/Integrative Nutrition/js/_impact.js
+++ themes/Integrative Nutrition/js/_impact.js
@@ -1,30 +1,25 @@
-/** 
+/**
  * If irclickid exists in query params set _irclickid cookie
  * If _irclickid cookie exists set it as a hidden field on all forms
  * */
-
 (() => {
+  const urlParams = new URLSearchParams(window.location.search);
+  let irclickid = urlParams.get('irclickid');
+  if (irclickid) {
+    IIN.cookies.setCookie('_irclickid', irclickid, 30);
+  } else {
+    irclickid = IIN.cookies.getCookieString('_irclickid');
+  }
 
-    const urlParams = new URLSearchParams(window.location.search);
-    let irclickid = urlParams.get('irclickid');
-    if (irclickid) {
-        const date = new Date();
-        date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000));
-        document.cookie = `_irclickid=${irclickid}; expires=${date.toGMTString()}; path=/`;
-    } else {
-        [irclickid] = (`; ${document.cookie}`).split(`; _irclickid=`).pop().split(';');
-    }
-
-    if (irclickid !== '') {
-        HubSpotForms.getForms().forEach((form) => {
-            form.onFormReady((formElement) => {
-                const input = document.createElement('input');
-                input.setAttribute('type', 'hidden');
-                input.setAttribute('name', 'irclickid');
-                input.setAttribute('value', irclickid);
-                formElement[0].appendChild(input);
-            })
-        })
-    }
-
-})()
+  if (irclickid !== '') {
+    HubSpotForms.getForms().forEach((form) => {
+      form.onFormReady((formElements) => {
+        const input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', 'irclickid');
+        input.setAttribute('value', irclickid);
+        formElements[0].appendChild(input);
+      });
+    });
+  }
+})();
