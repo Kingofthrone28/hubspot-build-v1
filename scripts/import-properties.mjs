13988829dc80d28471665efc29320cc62d519ada
Benjamin Gordon <benjamin.gordon8@icloud.com>
2024-09-12 17:19:12 -0400

    Import properties update (#362)
    * initial
    
    * initial forms commit
    
    * works without fieldGroups
    
    * works
    
    * update
    
    * lint and prettier
    
    * deconstruction
    
    * deconstructed
    
    * prettier
    
    * final
    
    * formRequests
    
    * fix linting and formatting issues
    
    * fixed (not ideal)
    
    * get import-forms.mjs outta here
    
    * prettier
    
    * fixed
    
    * fixed wait
    
    ---------
    
    Co-authored-by: Alex Orzeck-Byrnes <alex.orzeckbyrnes@integrativenutrition.com>

--- scripts/import-properties.mjs
+++ scripts/import-properties.mjs
@@ -43,6 +43,8 @@ async function getKeys() {
 async function runImport(prodKey, devKey) {
   hubspotClientProd = new hubspot.Client({ accessToken: prodKey });
   hubspotClientDev = new hubspot.Client({ accessToken: devKey });
+  const groups = await getPropertyGroups();
+  await postPropertyGroups(groups);
   try {
     const objectType = 'contacts';
     const archived = false;
@@ -95,18 +97,45 @@ function createContactObject({
 }
 
 async function postContacts(contacts) {
+  console.info('creating contacts... (not efficiently, please improve)');
   const objectType = 'contacts';
-  const formRequests = [];
-  try {
-    for (const contact of contacts) {
-      const apiResponse = hubspotClientDev.crm.properties.coreApi.create(
+  // const formRequests = [];
+
+  for (const contact of contacts) {
+    try {
+      const apiResponse = await hubspotClientDev.crm.properties.coreApi.create(
         objectType,
         contact,
       );
-      formRequests.push(apiResponse);
+    } catch (error) {
+      // console.error(contact.name + " exists");
     }
-    await Promise.allSettled(formRequests);
-  } catch (error) {
-    console.error(error);
+    // formRequests.push(apiResponse);
+  }
+  console.info('contact properties imported');
+  // await Promise.allSettled(formRequests);
+}
+
+async function getPropertyGroups() {
+  const objectType = 'contacts';
+  const apiResponse =
+    await hubspotClientProd.crm.properties.groupsApi.getAll(objectType);
+  const groups = apiResponse.results;
+  return groups;
+}
+
+async function postPropertyGroups(groups) {
+  const formRequests = [];
+  const objectType = 'contacts';
+
+  for (const group of groups) {
+    try {
+      const apiResponse = await hubspotClientDev.crm.properties.groupsApi.create(
+        objectType,
+        group,
+      );
+    } catch (error) {
+      //console.info(group.name + "already exists");
+    }
   }
 }
