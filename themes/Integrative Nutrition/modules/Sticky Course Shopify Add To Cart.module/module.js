bcf52465c91c116351bacde5acae104054ff3655
Sam Leavin <samuel.leavin@integrativenutrition.com>
2024-11-08 16:34:07 -0500

    cleanup

--- themes/Integrative Nutrition/modules/Sticky Course Shopify Add To Cart.module/module.js
+++ themes/Integrative Nutrition/modules/Sticky Course Shopify Add To Cart.module/module.js
@@ -124,36 +124,27 @@
     );
 
     const result = await IINShopifyClient.graphQLClient.send(query);
-    // console.log('result', result);
     const gql = result?.model?.products?.[0]
-    const product = await getProductData(productID);
+    gql.variants?.forEach(v => v.available = v.availableForSale)
     log('gqlProduct', gql)
-    log('product', product)
 
-    const optionsCount = IIN.shopify.getOptionsCount(product);
-    // log('optionsCount', IIN.shopify.getOptionsCount(gqlProduct) == optionsCount)
-    
+    const optionsCount = IIN.shopify.getOptionsCount(gql);
     configureDropdownHeading(optionsCount);
     matchPDPBottomSectionToTop(optionsCount);
-    const { productOptions, variantSelections } = processProduct(product);
     const { productOptions: gpo, variantSelections: gvs } = processProduct(gql);
-    // console.log('orig', productOptions, variantSelections)
-    // console.log('alt', gpo, gvs)
-    const availableVariants = IIN.shopify.getAvailableVariants(product);
-    // console.log('availableVariants', availableVariants.map(({id}) => id))
-    // console.log('alt', IIN.shopify.getAvailableVariants(gql).map(({id}) => id))
+    const availableVariants = IIN.shopify.getAvailableVariants(gql);
     const firstVariant = availableVariants?.[0];
     const discountInfo = await calculateDiscounts(firstVariant);
 
     handleSelectorChangeFull(
       moduleData,
-      product,
-      variantSelections,
-      productOptions,
+      gql,
+      gvs,
+      gpo,
       discountInfo,
     );
 
-    createViewItemEvent(product, firstVariant, moduleData);
+    createViewItemEvent(gql, firstVariant, moduleData);
   } catch (error) {
     console.error(error);
   }
