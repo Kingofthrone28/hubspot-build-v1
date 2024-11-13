2150e2ddffc50de4f3037ac4c634e32363ee45e6
Sam Leavin <samuel.leavin@integrativenutrition.com>
2024-11-13 11:25:09 -0500

    revert

--- themes/Integrative Nutrition/js/translation.js
+++ themes/Integrative Nutrition/js/translation.js
@@ -3,17 +3,14 @@
  * @see {@link https://developers.weglot.com/javascript/javascript#initialization-code WeGlot Getting Started}
  */
 (() => {
-  const keys = {
+  const weglotApiKeys = {
     www: 'wg_22f91c0430456d0d120ff3f524319edf0',
   };
 
   const { hostname } = window.location;
   const subdomains = hostname.split('.').slice(0, -2);
   const lastSubdomain = subdomains[subdomains.length - 1];
-  const subdomain = lastSubdomain || 'www';
-  const key =  keys[subdomain];
+  const weglotSubdomain = lastSubdomain || 'www';
 
-  if (key) {
-    Weglot.initialize({ api_key: key });
-  }
+  Weglot.initialize({ api_key: weglotApiKeys[weglotSubdomain] });
 })();
