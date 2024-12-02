839f80fb29fef8d51e97215f49d311db39667795
Hemanth Krishna <hemanth.krishna@brainlabsdigital.com>
2024-12-02 19:31:21 +0100

    Remove toggle for datalayer to production

--- themes/Integrative Nutrition/js/datalayer_ecomm.js
+++ themes/Integrative Nutrition/js/datalayer_ecomm.js
@@ -1,11 +1,9 @@
 window.dataLayer = window.dataLayer || [];
 
 const triggerECommEvent = async (rawPayload = {}) => {
-  if (IIN.cookies.checkCookie('analyticstest')) {
-    if (rawPayload && rawPayload.event) {
-      window.dataLayer.push({ ecommerce: null }); // As per GTM need-resetting
-      window.dataLayer.push(rawPayload);
-    }
+  if (rawPayload && rawPayload.event) {
+    window.dataLayer.push({ ecommerce: null }); // As per GTM need-resetting
+    window.dataLayer.push(rawPayload);
   }
 };
 
